3d2f76858b5c8b6877844da57ec3f8840ef400aa
Bruners <bruners@gmail.com>
2009-08-29 11:57:00 +0200

    try again

--- bTipCounter.lua
+++ bTipCounter.lua
@@ -15,27 +15,30 @@ end
 local lineAdded = false
 local function OnTooltipSetItem(tooltip, ...)
 	local _, link = GameTooltip:GetItem()
-	local _,itemLink,_,ilevel,_,_,_,maxstack,_,_ = link and GetItemInfo(link)
-	if not lineAdded then
-		if ilevel ~= nil then
-			if not disableIlevel and GetCVar("ShowItemLevel") == "0" then
-				tooltip:AddDoubleLine("Item level:", ilevel, 0,1,1,1,1,1)
-			end
-			if not disableItemCount then
-				local itemCount = GetItemCount(link, false)
-				local itemBankcount = GetItemCount(link, true) - itemCount
-				if itemCount + itemBankcount > 0 then
-					tooltip:AddDoubleLine("You have:", itemCount..(itemBankcount > 0 and (" (|cff88ffff+"..itemBankcount.."|r)") or ""), 0,1,1,1,1,1)
+	if link then
+		local _,itemLink,_,ilevel,_,_,_,maxstack,_,_ = GetItemInfo(link)
+		if not lineAdded then
+			if ilevel ~= nil then
+				if not disableIlevel and GetCVar("ShowItemLevel") == "0" then
+					tooltip:AddDoubleLine("Item level:", ilevel, 0,1,1,1,1,1)
 				end
-			end
-			if not disableStacksize then
-				if maxstack and maxstack > 1 then
-					tooltip:AddDoubleLine("Stack size:", maxstack,0,1,1,1,1,1)
+				if not disableItemCount then
+					local itemCount = GetItemCount(link, false)
+					local itemBankcount = GetItemCount(link, true) - itemCount
+					if itemCount + itemBankcount > 0 then
+						tooltip:AddDoubleLine("You have:", itemCount..(itemBankcount > 0 
+and (" (|cff88ffff+"..itemBankcount.."|r)") or ""), 0,1,1,1,1,1)
+					end
 				end
-			end
-			if not disableItemID then
-				if itemLink then
-					tooltip:AddDoubleLine("Item ID:", ToID(itemLink), 0,1,1,1,1,1)
+				if not disableStacksize then
+					if maxstack and maxstack > 1 then
+						tooltip:AddDoubleLine("Stack size:", maxstack,0,1,1,1,1,1)
+					end
+				end
+				if not disableItemID then
+					if itemLink then
+						tooltip:AddDoubleLine("Item ID:", ToID(itemLink), 0,1,1,1,1,1)
+					end
 				end
 			end
 		end
